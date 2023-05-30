5af65bb87cfdd0b2ef803796c353d24bf6fd9841
Mahfuz Bin Minhaz <mahfuzbinferdous@gmail.com>
2023-05-30 12:51:11 +0600

    Added a new program that factorialize a number by product of primes

---
+++ prime_factors.cpp
@@ -0,0 +1,59 @@
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <bitset>
+#include <array>
+#include <cstdint>
+using namespace std;
+
+#ifdef LOCAL 
+				#define eprintf(...) {fprintf(stderr, __VA_ARGS__); fflush(stderr); }
+#else 
+				#define eprintf(...) 42 
+#endif
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pil = pair<int, ll>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define fi first
+#define se second
+
+/* ########################### *
+ * # Welcome to the Internet # *
+ * #   Have a look around    # *
+ * #       @17sharat         # *
+ * ########################### */
+
+
+
+int main(int argc, char **argv) {
+	 
+	return 0;
+}
+
+
